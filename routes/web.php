45582c9a33795841a0e3971a03a624f6e6c891c9
RadenDimasErlangga21 <zirkified123@gmail.com>
2021-09-07 17:02:19 +0700

    Praktikum 2 - PageController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route:: get('/',[PageController::class,'indexLaravel']);
+
+Route:: get('/about',[PageController::class,'AboutUser']);
+
+Route:: get('/articles/{id}',[PageController::class,'ArticleClass']);
